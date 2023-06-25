ce35410be899716a9173cd0f3efb367b5b399527
saway11 <rgarcia20.m@gmail.com>
2023-06-24 20:25:15 -0700

    importing react added hero class for picture

---
+++ src/components/About/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import selfie from '';
+import './'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='content'>
+                <div className='rosa-pic title'>
+                    <img className="" src={selfie} alt="Rosa Picture" />
+                </div>
+                <div className="words">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
