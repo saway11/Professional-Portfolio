6f031c4b849482169fcbab6aaeb5bee61b16a192
saway11 <rgarcia20.m@gmail.com>
2023-06-25 06:21:26 -0700

    added footer w/ social media info

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css'
+const Footer = () => {
+
+    return <footer>
+        <div className='copyright'>
+            <p>Thank you</p>
+        </div>
+        <div className="social">
+            <a href="https://github.com/saway11" className='fa fa-github'> </a>
+            <a href="linkedin.com/in/rosa-maría-garcia-447937176" className='fa fa-linkedin'>\</a>
+        </div>
+    </footer>
+}
+
+export default Footer;
