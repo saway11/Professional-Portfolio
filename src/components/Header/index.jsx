b88d21405654f59c3dd9196f11d53fe850514a2a
saway11 <rgarcia20.m@gmail.com>
2023-06-25 06:43:20 -0700

    added function for header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css'
+
+const Header = (props) => {
+    const {
+        categories = [],
+        setCurrentCategory
+    } = props;
+
+    retrun (
+        <header>
+            <h1> Rosa Garcia </h1>
+            
+        </header>
+    )
+}
