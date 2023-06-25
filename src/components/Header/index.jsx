00ce08bc162430f008085ac7665fea382dfeb3bc
saway11 <rgarcia20.m@gmail.com>
2023-06-25 06:46:34 -0700

    completed header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,9 +7,23 @@ const Header = (props) => {
         setCurrentCategory
     } = props;
 
-    retrun (
+    return (
         <header>
             <h1> Rosa Garcia </h1>
+            <nav className='lg-nav'>
+                <ul>
+                    <p className="menu-btn">Menu</p>
+                    {categories.map((category) => {
+                        return <li className="menu-item" key={category.name}>
+                            <span style={{ cursor: 'pointer' }} className="menu-item" 
+                            onClick={() => {
+                                setCurrentCategory(category);
+                            }}
+                            >{category.name}</span>
+                        </li>
+                    })}
+                </ul>
+            </nav>
             
         </header>
     )
