00c1a2e2faa33d29488c4ccec5b107e546c66b1d
saway11 <rgarcia20.m@gmail.com>
2023-06-27 20:21:55 -0700

    deleted code

---
+++ src/components/Project/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import avengersProject from "../../images/";
+import bikiniBottonBonza from "../../images";
+import employeeManager from "../..";
+import noteTaker from "../..";
+import weatherDash from ".../";
+import blogger from '../..';
+import './style.css';
+
+
+
+const projects = [
+    {
+        name: 'avengersProject',
+        description: 'The Avengers website allows users to search for avenger characters and movies. The user will also have the ability to a movie of their interest to their watchlist.',
+        photoSrc: avengersProject,
+        url: 'https://rslaughter12.github.io/The-Avengers/Links.'
+    }
+]
