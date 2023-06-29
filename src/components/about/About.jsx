28f9b2deb728738ef945f8288b760f215bb365d2
saway11 <rgarcia20.m@gmail.com>
2023-06-28 17:05:00 -0700

    updated names on files, adding about section

---
+++ src/components/about/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+import Me from '../../assets/me.png';
+import {BiMedal, BiUser, BiFolder} from 'react-icons/bi';
+
+const About = () => {
+    return (
+        <section id='about'>
+            
+        </section>
+    )
+}
