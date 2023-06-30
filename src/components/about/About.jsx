30f6307ed82a533333deef801921d425d3ddfa0a
saway11 <rgarcia20.m@gmail.com>
2023-06-29 18:49:09 -0700

    added about cards

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,7 +6,25 @@ import {BiMedal, BiUser, BiFolder} from 'react-icons/bi';
 const About = () => {
     return (
         <section id='about'>
-            
+            <h5>Get to know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={ME} alt="A picture of Rosa"/>
+                    </div>
+                </div>
+                <div className="about__content">
+                    <div className="about__cards">
+                        <article className='about__card'>
+                            <BiMedal className='about__icon'/>
+                            <h5>Experience</h5>
+                            <small>Calaveras County Health and Human Services <br/> San Mateo Human Services</small>
+                        </article>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
