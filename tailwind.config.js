a39010766fe3bd0c29ce338014aaa79dfa5c2ce5
saway11 <rgarcia20.m@gmail.com>
2023-07-13 14:59:20 -0700

    configuration settings related to file paths, animations, and theme-related customization

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}"
+    ],
+    animation: {
+      float: "float 3s infinite",
+      refloat: "refloat 3s infinite"
+    },
+    float: {
+      '0%': {
+        transform: "translateY(0px)",
+        animationTimingFunction: "ease-in-out",
+      },
+      '50%': {
+        transform: "translateY(-20px)",
+        animationTimingFunction: "ease-in-out",
+      },
+      '100%': {
+        transform: "translateY(0px)",
+        animationTimingFunction: "ease-in-out",
+      } 
+    },
+    refloat: {
+      '0%': {
+        transform: "translateY(-20px)",
+        animationTimingFunction: "ease-in-out",
+      },
+      '50%': {
+        transform: "translateY(0px)",
+        animationTimingFunction: "ease-in-out",
+      },
+      '100%': {
+        transform: "translateY(-20px)",
+        animationTimingFunction: "ease-in-out",
+      } 
+    },
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
