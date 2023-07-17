8e19933c0b01430c7fa4d18ffd69d02803077497
saway11 <rgarcia20.m@gmail.com>
2023-07-17 09:53:08 -0700

    imported files and added styling

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -11,45 +11,45 @@ export default function Projects() {
             </p>
           </div>
         </div>
-                <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-1 px-4">
-                    <div className="shadow-md shadow-[#0404c16] hover:scale-110 duration-500 bg-blue-800 max-h-90 m-4">
-                        <img className="w-50" src="./img/project-1-img.jpg" alt="The Avengers icon" />
-                        <p className="my-5 text-xl">The Avengers</p>
-                        <p className="my-3 text-xl hover:text-orange-300"><a href="https://rslaughter12.github.io/The-Avengers" target="_blank" rel="noreferrer">Deployed Site</a></p>
-                        <p className="text-xl hover:text-orange-300"><a href="https://github.com/rslaughter12/The-Avengers" target="_blank" rel="noreferrer">Source Code</a></p>
-                    </div>
-                    <div className="shadow-md shadow-[#040c16 hover:scale-110 duration-500 bg-blue-800 max-h-90 m-3">
-                        <img className="w-50" src="./img/project-2-img.jpg" alt="Bikini Bottom Booking Bonanza icon" />
-                        <p className="my-5 text-xl">Bikini Bottom Booking Bonanza</p>
-                        <p className="my-3 text-xl hover:text-orange-300"><a href="https://bbbooking.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Site</a></p>
-                        <p className="text-xl hover:text-orange-300"><a href="https://github.com/Omarggm/BikiniBottomBooking" target="_blank" rel="noreferrer">Source Code</a></p>
-                    </div>
-                    <div className="shadow-md shadow-[#040c16 hover:scale-110 duration-500 bg-blue-800 max-h-90 m-3">
-                        <img className="w-50" src="./img/project-3-img.jpg" alt="Code Quiz icon" />
-                        <p className="my-5 text-xl">Code Quiz</p>
-                        <p className="my-3 text-xl hover:text-orange-300"><a href="https://saway11.github.io/code-quiz" target="_blank" rel="noreferrer">Deployed Site</a></p>
-                        <p className="text-xl hover:text-orange-300"><a href="https://github.com/saway11/code-quiz" target="_blank" rel="noreferrer">Source Code</a></p>
-                    </div>
-                    <div className="shadow-md shadow-[#040c16 hover:scale-110 duration-500 bg-blue-800 max-h-90 m-3">
-                        <img className="w-50" src="./img/project-4-img.jpg" alt="Weather Dashboard icon" />
-                        <p className="my-5 text-xl">Weather Dashboard</p>
-                        <p className="my-3 text-xl hover:text-orange-300"><a href="https://saway11.github.io/Weather-Dashboard" target="_blank" rel="noreferrer">Deployed Site</a></p>
-                        <p className="text-xl hover:text-orange-300"><a href="https://github.com/saway11/Weather-Dashboard" target="_blank" rel="noreferrer">Source Code</a></p>
-                    </div>
-                    <div className="shadow-md shadow-[#040c16 hover:scale-110 duration-500 bg-blue-800 max-h-90 m-3">
-                        <img className="w-50" src="./img/project-5-img.jpg" alt="Work Schedule icon" />
-                        <p className="my-5 text-xl">Work Schedule</p>
-                        <p className="my-3 text-xl hover:text-orange-300"><a href="https://saway11.github.io/Third-Party-APIs" target="_blank" rel="noreferrer">Deployed Site</a></p>
-                        <p className="text-xl hover:text-orange-300"><a href="https://github.com/saway11/Third-Party-APIs" target="_blank" rel="noreferrer">Source Code</a></p>
-                    </div>
-                    <div className="shadow-md shadow-[#040c16 hover:scale-110 duration-500 bg-blue-800 max-h-90 m-3">
-                        <img className="w-50" src="./img/project-5-img.jpg" alt="Note Taker icon" />
-                        <p className="my-5 text-xl">Note Taker</p>
-                        <p className="my-3 text-xl hover:text-orange-300"><a href="https://bbbooking.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Site</a></p>
-                        <p className="text-xl hover:text-orange-300"><a href="https://github.com/saway11/Note-Taker.git" target="_blank" rel="noreferrer">Source Code</a></p>
-                    </div>
-                </div>
-            </div>
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-1 px-14">
+          <div className="shadow-md shadow-[#efefef] hover:scale-110 duration-500 bg-blue-800 max-h-90 m-4">
+            <img className="w-50" src="./img/project-1-img.png" alt="The Avengers" />
+            <p className="my-5 text-xl">The Avengers</p>
+            <p className="my-3 text-xl hover:text-orange-300"><a href="https://rslaughter12.github.io/The-Avengers" target="_blank" rel="noreferrer">Deployed Site</a></p>
+            <p className="text-xl hover:text-orange-300"><a href="https://github.com/rslaughter12/The-Avengers" target="_blank" rel="noreferrer">Source Code</a></p>
+          </div>
+          <div className="shadow-md shadow-[#efefef] hover:scale-110 duration-500 bg-blue-800 max-h-90 m-3">
+            <img className="w-50" src="./img/project-2-img.png" alt="Bikini Bottom Booking" />
+            <p className="my-5 text-xl">Bikini Bottom Booking</p>
+            <p className="my-3 text-xl hover:text-orange-300"><a href="https://bbbooking.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Site</a></p>
+            <p className="text-xl hover:text-orange-300"><a href="https://github.com/Omarggm/BikiniBottomBooking" target="_blank" rel="noreferrer">Source Code</a></p>
+          </div>
+          <div className="shadow-md shadow-[#efefef] hover:scale-110 duration-500 bg-blue-800 max-h-90 m-4">
+            <img className="w-50" src="./img/project-3-img.png" target="_blank" alt="Code Quiz icon" />
+            <p className="my-5 text-xl">Code Quiz</p>
+            <p className="my-3 text-xl hover:text-orange-300"><a href="https://saway11.github.io/code-quiz" target="_blank" rel="noreferrer">Deployed Site</a></p>
+            <p className="text-xl hover:text-orange-300"><a href="https://github.com/saway11/code-quiz" target="_blank" rel="noreferrer">Source Code</a></p>
+          </div>
+          <div className="shadow-md shadow-[#efefef] hover:scale-110 duration-500 bg-blue-800 items-center m-4">
+            <img className="w-full max-h-56" src="./img/project-4-img.png" target="_blank" alt="Weather Dashboard icon" />
+            <p className="my-5 text-xl">Weather Dashboard</p>
+            <p className="my-3 text-xl hover:text-orange-300"><a href="https://saway11.github.io/Weather-Dashboard" target="_blank" rel="noreferrer">Deployed Site</a></p>
+            <p className="my-4 text-xl hover:text-orange-300"><a href="https://github.com/saway11/Weather-Dashboard" target="_blank" rel="noreferrer">Source Code</a></p>
+          </div>
+          <div className="shadow-md shadow-[#efefef] hover:scale-110 duration-500 bg-blue-800 items-center max-h-90 m-4">
+            <img className="w-full max-h-56" src="./img/project-5-img.png" target="_blank" alt="Work Schedule icon" />
+            <p className="my-4 text-xl">Work Schedule</p>
+            <p className="my-4 text-xl hover:text-orange-300"><a href="https://saway11.github.io/Third-Party-APIs" target="_blank" rel="noreferrer">Deployed Site</a></p>
+            <p className="my-4 text-xl hover:text-orange-300"><a href="https://github.com/saway11/Third-Party-APIs" target="_blank" rel="noreferrer">Source Code</a></p>
+          </div>
+          <div className="shadow-md shadow-[#efefef] hover:scale-110 duration-500 bg-blue-800 items-center m-4">
+            <img className="w-full h-56" src="./img/project-6-img.png" target="_blank" alt="Note Taker icon" />
+            <p className="my-5 text-xl">Note Taker</p>
+            <p className="my-5 text-xl hover:text-orange-300"><a href="https://bbbooking.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Site</a></p>
+            <p className="my-3 text-xl hover:text-orange-300"><a href="https://github.com/saway11/Note-Taker.git" target="_blank" rel="noreferrer">Source Code</a></p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-odule.exports = {
+module.exports = {
     content: [
       "./src/**/*.{js,jsx,ts,tsx}"
     ],
